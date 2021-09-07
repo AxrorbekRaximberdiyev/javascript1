dac0d48bca2a1b009d2ff59a9ddeac73531b2250
AxrorbekRaximberdiyev <araximberdiyev896@gmail.com>
2021-09-07 22:17:32 +0500

    inital commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+var welcome = alert("Hush kelibsiz!");
+var firsName = prompt("ismingizni kiriting:").trim();
+var sureName = prompt("Familiyangizni kiriting:").trim();
+var yourInformation= prompt("Ma'lumotingizni Ma'lumotingizkiriting:").trim();
+var age = prompt("yoshingizni kiriting:").trim();
+var phoneNumber = prompt("telefon raqamingizni kiriting:").trim();
+console.log(`alert: ${welcome}\nismingiz: ${firsName} \nfamiliyangiz: ${sureName}\nMa'lumotingiz: ${yourInformation}\nyoshingiz: ${age}\ntefon raqamingiz: ${phoneNumber}`);
+
+
+
